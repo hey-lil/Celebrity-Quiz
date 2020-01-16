ce80fb301193da254d77642da43f8326d66d694d
Jaki Widby and Lillian Taft <jaki@>
2020-01-16 11:41:12 -0800

    add form submission and form return options

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
-var time = $("input:radio[name=time]:checked").val();
-var bathe = $("input:radio[name=bathe]:checked").val();
-var dinner = $("input:radio[name=dinner]:checked").val();
+
+$(document).ready(function() {
+  $("#submit").submit(function(event) {
+    var time = $("input:radio[name=time]:checked").val();
+    var bathe = $("input:radio[name=bathe]:checked").val();
+    var dinner = $("input:radio[name=dinner]:checked").val();
+
+    if (time === lateNight && bathe === bath && dinner === potRoast) {
+      $().show();
+    }
+    if (time === lateNight && bathe === bath && dinner === potRoast) {
+      $().show();
+    }
+    if (time === lateNight && bathe === bath && dinner === potRoast) {
+      $().show();
+    }
+  }
+}
