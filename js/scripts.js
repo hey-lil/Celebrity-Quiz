1dc8c2ce526a998b64abfa2c9477c6f3d1b78a8a
Jaki Widby and Lillian Taft <jaki@>
2020-01-16 13:34:04 -0800

    fix missing parentesis in if statement and add form to html

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,22 @@
 
 $(document).ready(function() {
-  $("#submit").submit(function(event) {
+  $("form#questions").submit(function() {
+    event.preventDefault();
+
     var time = $("input:radio[name=time]:checked").val();
     var bathe = $("input:radio[name=bathe]:checked").val();
     var dinner = $("input:radio[name=dinner]:checked").val();
 
-    if (time === lateNight && bathe === bath && dinner === potRoast) {
+    if (time === "lateNight" && bathe === "bath" && dinner === "potRoast") {
       $("#Answer").show();
       $("#shirley").show();
     }
 
-    else if (time === midDay && bathe === bath && dinner === breakfast {
+    else if (time === "midDay" && bathe === "bath" && dinner === "breakfast") {
       $("#Answer").show();
       $("#sandra").show();
     }
-    else if (time === evening && bathe === shower && dinner === pizza) {
+    else if (time === "evening" && bathe === "shower" && dinner === "pizza") {
       $("#Answer").show();
       $("#kate").show();
     }
@@ -22,5 +24,6 @@ $(document).ready(function() {
       $("#Answer").show();
       $("#kristen").show();
     }
-  }
-}
+
+  });
+});
